b6291e1d5cdcdf19772bff03246e626d2630bdc2
Andersonv16 <andersonviana50@gmail.com>
2023-01-26 03:26:09 -0300

    validação de data de nascimento

--- js/valida-idade.js
+++ js/valida-idade.js
@@ -4,7 +4,6 @@ export default function ehMaiorDeIdade(campo) {
 
     console.log(validaIdade(dataNascimento));
 }
-
 function validaIdade(data) {
     const dataAtual = new Date();
     const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
